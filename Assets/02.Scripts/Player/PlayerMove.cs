276f12392bcdd2e2cb7a11a08a39b7d5aa751ca4
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-04-21 14:49:59 +0900

    Added Player jump function

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float MoveSpeed = 7f;
+    public float JumpPower = 5f;
+
+    private CharacterController _characterController;
+    private const float GRAVITY = -9.8f; // 중력
+    private float _yVelocity = 0f;       // 중력 가속도
+
+    private bool _isJumping = false;
     /* 목표 : wasd를 누르면 캐릭터가 이동 (카메라 방향에 맞게)
      * 
      * 구현 순서 : 
@@ -10,6 +17,11 @@ public class PlayerMove : MonoBehaviour
      * 2. 입력으로부터 방향 설정
      * 3. 방향에 따라 플레이어 이동
      */
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
@@ -19,9 +31,21 @@ public class PlayerMove : MonoBehaviour
         Vector3 dir = new Vector3(h, 0, v);
         dir = dir.normalized;
         dir = Camera.main.transform.TransformDirection(dir);
-        dir.y = 0;
+        //dir.y = 0;
         //TransformDirection : 로컬 공간의 벡터 -> 월드 공간의 벡터
 
-        transform.position += dir * MoveSpeed * Time.deltaTime;
+        if (_characterController.isGrounded) _isJumping = false;
+
+        if (Input.GetButtonDown("Jump") && !_isJumping)
+        {
+            _yVelocity = JumpPower;
+            _isJumping = true;
+        }
+
+        _yVelocity += GRAVITY * Time.deltaTime;
+        dir.y = _yVelocity;
+
+        //transform.position += dir * MoveSpeed * Time.deltaTime;
+        _characterController.Move(dir * MoveSpeed * Time.deltaTime);
     }
 }
