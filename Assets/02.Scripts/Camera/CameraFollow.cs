0b69881fc62f8136f2263d60abe92f8fd98238e8
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-04-22 09:52:55 +0900

    Added viewpoint change

--- Assets/02.Scripts/Camera/CameraFollow.cs
+++ Assets/02.Scripts/Camera/CameraFollow.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform Target;
+    public Transform[] Targets;
+    public int ViewPoint = 0;
 
     private void Update()
     {
         // interpoling, smoothing ±â¹ý
-        transform.position = Target.position;
+        transform.position = Targets[ViewPoint].position;
+
+        if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            ViewPoint = 0;
+        } else if(Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            ViewPoint = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            ViewPoint = 2;
+        }
     }
 }
