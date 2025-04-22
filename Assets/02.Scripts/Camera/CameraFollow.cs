13cdca8913792ace6b0c6f53f9f606604deabaa4
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-04-22 10:55:40 +0900

    Camera now revolves around player

--- Assets/02.Scripts/Camera/CameraFollow.cs
+++ Assets/02.Scripts/Camera/CameraFollow.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -5,6 +6,35 @@ public class CameraFollow : MonoBehaviour
     public Vector3[] Targets;
     public Transform Player;
     public int ViewPoint = 0;
+
+    private void Revolve()
+    {
+        float xdegree = Player.eulerAngles.y;
+
+        float yawRad = xdegree * Mathf.Deg2Rad;
+
+        float x = Mathf.Sin(yawRad) * Targets[ViewPoint].z;
+        float z = Mathf.Cos(yawRad) * Targets[ViewPoint].z;
+
+        transform.position = Player.position + new Vector3(x, Targets[ViewPoint].y, z);
+        Debug.Log($"camera is here : {transform.position}");
+    }
+
+    private void ViewSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            ViewPoint = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            ViewPoint = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            ViewPoint = 2;
+        }
+    }
 
     private void Awake()
     {
@@ -14,18 +44,7 @@ public class CameraFollow : MonoBehaviour
     private void Update()
     {
         // interpoling, smoothing ±â¹ý
-        transform.position = Player.position + Targets[ViewPoint];
-
-        if (Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8))
-        {
-            ViewPoint = 0;
-        } else if(Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
-        {
-            ViewPoint = 1;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            ViewPoint = 2;
-        }
+        ViewSwitch();
+        Revolve();
     }
 }
