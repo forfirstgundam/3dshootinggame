b601c8b27d0271b7960100c3cd515644dc964b8d
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-04-23 17:16:55 +0900

    Backlog creation works

--- Assets/02.Scripts/UI/BacklogUI.cs
+++ Assets/02.Scripts/UI/BacklogUI.cs
@@ -1,25 +1,42 @@
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BacklogUI : MonoBehaviour
 {
     public static BacklogUI Instance;
     public TextMeshProUGUI BacklogPrefab;
+    public Transform ContentParent;
+
+    public int MaxLogs = 5;
+
+    private Queue<GameObject> _logs = new Queue<GameObject>();
 
     private void Awake()
     {
         Instance = this;
     }
 
-    public void Backlog(string str)
+    public void AddLog(string message)
     {
-        TextMeshProUGUI temp = Instantiate(BacklogPrefab);
-        temp.text = str;
-        temp.transform.SetParent(this.transform);
+        // 초과되면 가장 오래된 로그 제거
+        if (_logs.Count >= MaxLogs)
+        {
+            GameObject oldLog = _logs.Dequeue();
+            Destroy(oldLog);
+        }
+
+        // 새 로그 생성
+        TextMeshProUGUI newLog = Instantiate(BacklogPrefab, ContentParent);
+        newLog.text = message;
+        _logs.Enqueue(newLog.gameObject);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)) Backlog("테스트용입니다");
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AddLog("테스트 로그입니다.");
+        }
     }
 }
