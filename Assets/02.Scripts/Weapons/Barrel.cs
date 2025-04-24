368fea095b24952b2392cee88b52a2f904babda9
forfirstgundam <90223382+forfirstgundam@users.noreply.github.com>
2025-04-24 16:59:11 +0900

    Barrel does damage

--- Assets/02.Scripts/Weapons/Barrel.cs
+++ Assets/02.Scripts/Weapons/Barrel.cs
@@ -8,14 +8,20 @@ public class Barrel : MonoBehaviour
 {
     public int MaxHealth = 50;
     public int CurHealth;
+    public ParticleSystem Explosion;
 
     public float HitTime = 0.05f;
     public float FlyTime = 5f;
 
     public float ExplodePower = 50f;
+    public float ExplosionRadius = 10f;
 
     private Coroutine BeingHit;
 
+    private void Awake()
+    {
+        Initialize();
+    }
     public void Initialize()
     {
         CurHealth = MaxHealth;
@@ -39,6 +45,27 @@ public class Barrel : MonoBehaviour
         float _timer = 0f;
         dir.y += ExplodePower;
         dir = dir.normalized;
+        Instantiate(Explosion, transform.position, Quaternion.identity);
+
+        Damage damage = new Damage();
+        damage.Value = 50;
+        damage.From = this.gameObject;
+        damage.KnockValue = 0.2f;
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, ExplosionRadius, layerMask:8);
+        foreach (var hitCollider in hitColliders)
+        {
+            Enemy enemy = hitCollider.GetComponent<Enemy>();
+            damage.KnockDir = (hitCollider.transform.position - transform.position).normalized;
+            enemy.TakeDamage(damage);
+        }
+        Collider[] PlayerhitCollider = Physics.OverlapSphere(transform.position, ExplosionRadius, layerMask: 6);
+        //foreach (var hitCollider in hitColliders)
+        //{
+        //    PlayerMove player = hitCollider.GetComponent<PlayerMove>();
+        //    damage.KnockDir = (hitCollider.transform.position - transform.position).normalized;
+        //    PlayerMove.TakeDamage(damage);
+        //}
         while (_timer <= FlyTime)
         {
             transform.Translate(dir * knockback * ExplodePower * Time.deltaTime);
